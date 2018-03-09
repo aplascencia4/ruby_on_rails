91f5a14b7d0a359202b858a470fbb3bdc607f449
Angel Plascencia <aplascencia4@cps.edu>
2018-03-09 20:31:28 +0000

    Add dynamic route segments

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,10 +1,9 @@
 class TodoController < ApplicationController
-  
   def index
   end
   
   def show
-  @todo_description = "Make the curriculum"
-  @todo_pomodoro_estimate = 4
+    @todo_description = "variable"
+    @todo_pomodoro_estimate = 4
   end
 end
