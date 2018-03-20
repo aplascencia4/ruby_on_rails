eb120282aaacc257c6dd5c2bac67202d44a8d0ac
Angel Plascencia <aplascencia4@cps.edu>
2018-03-20 13:25:16 +0000

    Add the RCAV flows for the new and create actions

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,16 +3,18 @@ class TodoController < ApplicationController
   end
   
   def show
-  todo_id = params[:id]
   @todo = Todo.find_by_id(params[:id])
+  end
   
-  if todo_id == '1' # Remember, the `==` conditional operator is used to check if two things are equal to each other.
-    @todo_description = "Make the curriculum"
-    @todo_pomodoro_estimate = 4
-    
-  elsif todo_id == '2'
-    @todo_description = "Buy workshop supplies"
-    @todo_pomodoro_estimate = 3
+  def new
   end
+  
+  def create
+    t = Todo.new
+    t.description = params['description']
+    t.pomodoro_estimate = params['pomodoro_estimate']
+    t.save
+    redirect_to "/todo/show/#{ t.id }"
   end
+
 end
